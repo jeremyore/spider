a426561670dfa7001cedcfea548b015fbb09d1fe
wangjing <wangjing7203@fiberhome.com>
2018-01-10 15:35:16 +0800

    增加jd爬取

---
+++ src/main/java/com/it/jd/App.java
@@ -0,0 +1,54 @@
+package com.it.jd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        save_1_Item();
+
+        //saveAllPagesItems();
+    }
+
+    public static void save_1_Item() {
+        String path = "/home/wangjing/Downloads/商品数据/歌浪香品/";
+
+        List<String> urlList = new ArrayList<>();
+        urlList.add("https://item.jd.com/18897887801.html");
+        urlList.add("https://item.jd.com/19489037465.html");
+        urlList.add("https://item.jd.com/18912228390.html");
+        urlList.add("https://item.jd.com/18847961807.html");
+        urlList.add("https://item.jd.com/18913180555.html");
+
+        urlList.add("https://item.jd.com/18815253605.html");
+        urlList.add("https://item.jd.com/18913947332.html");
+        urlList.add("https://item.jd.com/18814027441.html");
+        urlList.add("https://item.jd.com/24151153343.html");
+        urlList.add("https://item.jd.com/18845073203.html");
+
+        urlList.add("https://item.jd.com/18914363353.html");
+        urlList.add("https://item.jd.com/18848075603.html");
+
+
+
+        for (String url : urlList) {
+            SpiderUtils.saveDetailItems(url, path);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void saveAllPagesItems() {
+        List<String[]> arrayList = new ArrayList<>();
+        arrayList.add(new String[]{"https://mall.jd.com/view_search-813393-0-99-1-24-", "/home/wangjing/Downloads/商品数据/歌浪香品/", "1", ".html"});
+
+
+        for (String[] strArr : arrayList) {
+            SpiderUtils.saveAllPagesItems(strArr[0], strArr[1], Integer.parseInt(strArr[2]), strArr[3]);
+
+        }
+    }
+}
---
+++ src/main/java/com/it/jd/SpiderUtils.java
@@ -0,0 +1,188 @@
+package com.it.jd;
+
+import com.it.utils.DownImgUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SpiderUtils {
+
+    //爬取详情页
+    public static void saveDetailItems(String url, String path) {
+        try {
+
+            Document curDoc = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0")
+                    .timeout(3000)
+                    .get();
+
+            String itemName = getItemName(curDoc);
+            String filePath = path + itemName + "/参数.txt";
+
+            List<String> itemNameList = new ArrayList<>();
+            itemNameList.add("商品链接:");
+            itemNameList.add(url);
+            itemNameList.add("======================================");
+            itemNameList.add("商品名称:");
+            itemNameList.add(itemName);
+            itemNameList.add("======================================");
+            FileUtils.writeLines(new File(filePath), "UTF-8", itemNameList, true);
+            System.out.println(itemNameList);
+
+       /*     List<String> colorList = getColors(curDoc, path + itemName);
+            FileUtils.writeLines(new File(filePath), "UTF-8", colorList, true);
+            System.out.println(colorList);*/
+
+            List<String> paraList = getProductPara(curDoc);
+            FileUtils.writeLines(new File(filePath), "UTF-8", paraList, true);
+            System.out.println(paraList);
+
+            List<String> titleImgsUrl = getTitleImgsURL(curDoc);
+            DownImgUtils.saveImgList(titleImgsUrl, path + itemName, "title");
+
+            List<String> descImgsUrl = getDescImgsURL(curDoc);
+            DownImgUtils.saveImgList(descImgsUrl, path + itemName, "desc");
+        } catch (Exception e) {
+            System.out.println("saveDetailItems错误:" + e.getMessage());
+        }
+    }
+
+    public static String getItemName(Document curDoc) {
+        String itemName = curDoc.select("div.sku-name").text();
+        return itemName;
+    }
+
+    public static List<String> getColors(Document curDoc, String path) {
+        List<String> colorList = new ArrayList<>();
+        colorList.add("颜色种类:");
+        Elements colorEles = curDoc.select("#J_DetailMeta > div.tm-clear > div.tb-property > div > div.tb-key > div > div > dl.tb-prop.tm-sale-prop.tm-clear.tm-img-prop > dd > ul > li");
+        for (Element element : colorEles) {
+            String colorName = element.attr("title");
+            colorList.add(colorName);
+            //颜色对应图片下载
+            String backgroundUrl = element.child(0).attr("style");
+            Pattern p = Pattern.compile("(//img.*.jpg)");
+            Matcher m = p.matcher(backgroundUrl);
+            if (m.find()) {
+                String colorImgURL = "https:" + m.group(1);
+                colorImgURL = colorImgURL.replace("40x40", "430x430");
+                DownImgUtils.saveImg(colorImgURL, path, colorName + ".jpg");
+            }
+        }
+        colorList.add("======================================");
+        return colorList;
+    }
+
+    public static List<String> getTitleImgsURL(Document curDoc) {
+        List<String> imgsURL = new ArrayList<>();
+        Elements titleImgEles = curDoc.select("#spec-list > ul > li > img");
+        for (Element element : titleImgEles) {
+            //替换成大图
+            String imgUrl = "https:" + element.attr("src").replace("n5", "n1");
+            imgsURL.add(imgUrl);
+        }
+        return imgsURL;
+    }
+
+    public static List<String> getProductPara(Document curDoc) {
+        List<String> productParaList = new ArrayList<>();
+        productParaList.add("详细参数:");
+        Elements productParaEles = curDoc.select("#detail > div.tab-con > div:nth-child(1) > div.p-parameter > ul.parameter2.p-parameter-list > li");
+        for (Element element : productParaEles) {
+            productParaList.add(element.text());
+        }
+        productParaList.add("======================================");
+        return productParaList;
+    }
+
+    public static List<String> getDescImgsURL(Document curDoc) {
+        List<String> descImgsUrlList = new ArrayList<>();
+        Pattern pDesc = Pattern.compile("desc:\\s*'(.*?)'");
+        Matcher mDesc = pDesc.matcher(curDoc.outerHtml());
+        if (mDesc.find()) {
+            String descImgURL = "https:" + mDesc.group(1);
+            try {
+                Document descImgDoc = Jsoup.connect(descImgURL)
+                        .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0")
+                        .ignoreContentType(true)
+                        .timeout(3000)
+                        .get();
+
+                String descText = StringUtils.replaceChars(descImgDoc.outerHtml(), "\\", "");
+                descText = StringUtils.removePattern(descText, "&quot;");
+
+
+                //过滤处jpg大图片
+                Pattern pImg = Pattern.compile("data-lazyload=\"(.*?)\"");
+                Matcher mImg = pImg.matcher(descText);
+                int imgIndex = 0;
+                while (mImg.find()) {
+                    if (mImg.group(1).endsWith("jpg")) {
+                        imgIndex += 1;
+                        descImgsUrlList.add("https:" + mImg.group(1));
+                    }
+
+                }
+
+            } catch (Exception e) {
+                System.out.println("getDescImgsURL错误:" + e.getMessage());
+            }
+        }
+        return descImgsUrlList;
+    }
+
+
+    //==============================================================
+    //分页爬取全店
+    //获取全店所有商品
+    public static Set<String> getAllPages(String url, int pagesNum, String catUrl) {
+        Set<String> itemUrlSet = new HashSet<>();
+        try {
+            Document descDoc = Jsoup.connect("https://module-jshop.jd.com/module/getModuleHtml.html?appId=813393&orderBy=99&pageNo=1&direction=1&categoryId=0&pageSize=24&pagePrototypeId=8&pageInstanceId=84209352&moduleInstanceId=84209355&prototypeId=55555&templateId=905542&layoutInstanceId=84209355&origin=0&shopId=725756&venderId=730730&callback=jshop_module_render_callback&_=1515563921165")
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0")
+                    .ignoreContentType(true)
+                    .timeout(3000)
+                    .get();
+            //String descUrl = url + curDoc.select("#J_ShopAsynSearchURL").attr("value");
+            //for (int i = 1; i <= pagesNum; i++) {
+            //String fullDescUrl = descUrl + "&pageNo=" + i;
+//                Document descDoc = Jsoup.connect(fullDescUrl)
+//                        .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0")
+//                        .referrer("https://wfjn.tmall.com")
+//                        .header("cookie", "cna=aSLOEpMnFnwCAatRTwa37Zev; _m_h5_tk=8fa5c775ac5dc879c399df36902ef7ce_1514618129404; _m_h5_tk_enc=395aa496fffbfeea113cdb7006ec738e; _uab_collina=151461615393926348635204; hng=CN%7Czh-CN%7CCNY%7C156; uc1=cookie14=UoTdf1eg5d%2FbFA%3D%3D&lng=zh_CN&cookie16=UIHiLt3xCS3yM2h4eKHS9lpEOw%3D%3D&existShop=true&cookie21=UtASsssmfaCONGki4KTH3w%3D%3D&tag=8&cookie15=UIHiLt3xD8xYTw%3D%3D&pas=0; uc3=sg2=BdKPbwp%2BZ2Bu77djrIdNx9G89sCsABJFeIu77idJ5ME%3D&nk2=s3HdEsfI&id2=UNJXw5DtIMP%2F&vt3=F8dBzLeP1WfS2QCMCsM%3D&lg2=W5iHLLyFOGW7aA%3D%3D; tracknick=%5Cu8F69%5Cu96E8%5Cu5915; _l_g_=Ug%3D%3D; unb=326520558; lgc=%5Cu8F69%5Cu96E8%5Cu5915; cookie1=VqpePiHblzYCDdQVKH4rzY5102lKcahg47Kp9e6BVHQ%3D; login=true; cookie17=UNJXw5DtIMP%2F; cookie2=2a37ac9ed2843eb2b0484b864e55bb53; _nk_=%5Cu8F69%5Cu96E8%5Cu5915; uss=URsUssD1NJgyzNM0iZJrzPQj4mV7D%2BWplOfutYcPphG1zzQYByof2Xj3; sg=%E5%A4%958c; t=1a6dda88319afc0d9eac6e843b46e2ac; _tb_token_=e6e3156ae5350; x=__ll%3D-1%26_ato%3D0; cq=ccp%3D0; swfstore=313512; _umdata=486B7B12C6AA95F247DC96710A6775A5E63A6D169A60D233D7A7432A45D083B2D272A7D0677A412ECD43AD3E795C914C39BC92ED903147A865F887EFD57281B3; pnm_cku822=; otherx=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0; whl=-1%260%260%260; isg=AsnJJAN39z_JR4tL74KQ9rM82PXjvrw9n2pHXGs-cLD3sunEs2bNGLfggCH-")
+//                        .timeout(3000)
+//                        .get();
+
+            String descText = StringUtils.replaceChars(descDoc.toString(), "\\", "");
+            descText = StringUtils.removePattern(descText, "&quot;");
+            descDoc = Jsoup.parse(descText);
+            Elements itemEles = descDoc.select("#J_GoodsList > ul > li > div > div.jPic > a");
+            for (Element element : itemEles) {
+                itemUrlSet.add("https:" + element.attr("href"));
+            }
+            // }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return itemUrlSet;
+    }
+
+    public static void saveAllPagesItems(String url, String path, int pagesNum, String catUrl) {
+        Set<String> itemUrlSet = getAllPages(url, pagesNum, catUrl);
+        for (String itemUrl : itemUrlSet) {
+            saveDetailItems(itemUrl, path);
+            System.out.println(itemUrl);
+        }
+    }
+}
